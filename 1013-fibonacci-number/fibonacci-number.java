b6bcec5a77ea7a47595378a8486ee6a8f4f90ce6
RAHUL YADAV <rairahul5505@gmail.com>
2026-07-01 16:35:36 +0530

    Time: 23 ms (8.72%) | Memory: 46.2 MB (15.73%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int fib(int n) {
+        if(n<=0){
+            return 0;
+        }
+        if(n<=1){
+            return n;
+        }
+        int[] dp = new int[n+1];
+        if(dp[n] == n){return dp[n];}
+        int first = fib(n-1);
+        int last = fib(n-2);
+        return first + last;
+    }
+    public static void main(String[] args){
+        int n = 2;
+        int fibSum = fib(n);
+        System.out.println(fibSum); 
+    }
+}
