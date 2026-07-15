08a0cb5b68891c8c22d7f59e3c943d26002eca3e
RAHUL YADAV <rairahul5505@gmail.com>
2026-07-15 13:36:04 +0530

    Time: 1 ms (49.77%) | Memory: 43.2 MB (10.23%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -4,36 +4,42 @@ class Solution {
     static final int BEN = 1;
 
     public boolean predictTheWinner(int[] nums) {
-
+        int n = nums.length;
         int total = 0;
         for (int x : nums)
             total += x;
 
-        int ariaScore = helper(nums, 0, nums.length - 1, ARIA);
+        int[][][] dp = new int[n+1][n+1][2];
+       for (int[][] matrix : dp) {
+    for (int[] row : matrix) {
+        Arrays.fill(row, -1);
+    }
+}
+        int ariaScore = helper(nums, 0, nums.length - 1, ARIA,dp);
 
         return ariaScore >= total - ariaScore;
     }
 
-    public int helper(int[] nums, int left, int right, int turn) {
+    public int helper(int[] nums, int left, int right, int turn,int[][][] dp) {
 
         if (left > right)
             return 0;
-
+            if(dp[left][right][turn] != -1) return dp[left][right][turn];
         if (turn == ARIA) {
 
-            int takeLeft = nums[left] + helper(nums, left + 1, right, BEN);
+            int takeLeft = nums[left] + helper(nums, left + 1, right, BEN,dp);
 
-            int takeRight = nums[right] + helper(nums, left, right - 1, BEN);
+            int takeRight = nums[right] + helper(nums, left, right - 1, BEN,dp);
 
-            return Math.max(takeLeft, takeRight);
+            return dp[left][right][turn] = Math.max(takeLeft, takeRight);
 
         } else {
 
-            int takeLeft = helper(nums, left + 1, right, ARIA);
+            int takeLeft = helper(nums, left + 1, right, ARIA,dp);
 
-            int takeRight = helper(nums, left, right - 1, ARIA);
+            int takeRight = helper(nums, left, right - 1, ARIA,dp);
 
-            return Math.min(takeLeft, takeRight);
+            return dp[left][right][turn] =  Math.min(takeLeft, takeRight);
 
         }
     }
