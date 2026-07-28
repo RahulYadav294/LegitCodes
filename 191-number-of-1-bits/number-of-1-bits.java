0d94afb76a0f3fe2c37ad0bb32b149f79a7243fc
RAHUL YADAV <rairahul5505@gmail.com>
2026-07-28 13:26:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (48.28%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,6 +1,36 @@
 class Solution {
     public int hammingWeight(int n) {
+        int count = 0;
+       while(n != 0){
+            n = n & (n-1);
+                count++;
+            
+        }
+        return count;
+        
+    }
+}
+
+
+/*
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        for(int i = 0; i<32; i++){
+            if((n & (1<<i)) != 0){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+}
+*/
+/*
+class Solution {
+    public int hammingWeight(int n) {
         return Integer.bitCount(n);
         
     }
 }
+*/
