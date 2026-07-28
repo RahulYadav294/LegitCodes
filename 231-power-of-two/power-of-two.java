567bef7507c12a3b5c8cb7099e2f679c318750cf
RAHUL YADAV <rairahul5505@gmail.com>
2026-07-28 13:27:20 +0530

    Time: 1 ms (96.65%) | Memory: 42.4 MB (87.74%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static boolean isPowerOfTwo(int n) {
+        if(n == 1){
+            return true;
+        }
+        if(n<=0 || n%2!=0){
+            return false;
+        }
+        return isPowerOfTwo(n/2);
+    }
+}
