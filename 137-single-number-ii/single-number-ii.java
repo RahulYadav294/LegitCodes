2ab734c1779a7a993760701ea7e89e06afee6464
RAHUL YADAV <rairahul5505@gmail.com>
2026-07-29 19:02:58 +0530

    Time: 4 ms (64.86%) | Memory: 45 MB (86.07%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,5 +1,24 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int bit = 0; bit<32; bit++){
+            int count = 0;
+            for(int num : nums){
+                if((num & (1<<bit)) != 0){
+                    count++;
+                }
+            }
+            if(count % 3 != 0){
+                    ans|=(1<<bit);
+                }
+        }
+        return ans;
+    }
+}
+
+/*
+class Solution {
+    public int singleNumber(int[] nums) {
         Map<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i<nums.length; i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
@@ -10,3 +29,4 @@ class Solution {
         return -1;
     }
 }
+*/
