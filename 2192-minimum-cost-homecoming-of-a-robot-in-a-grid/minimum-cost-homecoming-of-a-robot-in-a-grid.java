15051611cf5066e7b4ee3d78ec98afd6acba158a
RAHUL YADAV <rairahul5505@gmail.com>
2026-07-31 14:53:59 +0530

    Time: 2 ms (58.23%) | Memory: 130.4 MB (24.68%) - LeetSync

---
+++ 2192-minimum-cost-homecoming-of-a-robot-in-a-grid/minimum-cost-homecoming-of-a-robot-in-a-grid.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minCost(int[] startPos, int[] homePos, int[] rowCosts, int[] colCosts) {
+        int cost = 0;
+        if(startPos[0] < homePos[0]){
+            for(int r = startPos[0] + 1; r<=homePos[0]; r++){
+                cost+=rowCosts[r];
+            }
+            }else{
+                for(int r = startPos[0]-1; r>=homePos[0]; r--){
+                    cost+=rowCosts[r];
+                }
+            }
+        
+        if(startPos[1] < homePos[1]){
+            for(int c = startPos[1]+1; c<=homePos[1]; c++){
+                cost+=colCosts[c];
+            }
+            }else{
+                for(int c = startPos[1]-1; c>=homePos[1]; c--){
+                    cost+=colCosts[c];
+                }
+            }
+        return cost;
+        
+    }
+}
