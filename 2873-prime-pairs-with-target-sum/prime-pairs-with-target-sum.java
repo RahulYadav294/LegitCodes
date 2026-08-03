d317cb9c6da90a8f5718815a64fe9f098bc7fef1
RAHUL YADAV <rairahul5505@gmail.com>
2026-08-03 16:23:07 +0530

    Time: 1972 ms (7.57%) | Memory: 53.4 MB (90.84%) - LeetSync

---
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<List<Integer>> findPrimePairs(int n) {
+        List<List<Integer>> bigList = new ArrayList<>();
+        for(int i = 2; i<=n/2; i++){
+            int j = n-i;
+                if(isPrime(i) && isPrime(j)){
+                    int sum = i + j;
+                    if(sum == n){
+                        bigList.add(Arrays.asList(i,j));
+                    }
+                }
+        }
+        return bigList;
+    }
+    public boolean isPrime(int x){
+         if (x < 2) return false;
+        for(int i = 2; i*i<=x; i++){
+            if(x % i == 0) return false;
+        }
+        return true;
+    }
+}
+/*
+class Solution {
+    public List<List<Integer>> findPrimePairs(int n) {
+        List<List<Integer>> bigList = new ArrayList<>();
+        for(int i = 2; i<=n; i++){
+            for(int j = i; j<=n; j++){
+                if(isPrime(i) && isPrime(j)){
+                    int sum = i + j;
+                    if(sum == n){
+                        bigList.add(Arrays.asList(i,j));
+                    }
+                }
+            }
+        }
+        return bigList;
+    }
+    public boolean isPrime(int x){
+        for(int i = 2; i*i<=x; i++){
+            if(x % i == 0) return false;
+        }
+        return true;
+    }
+}
+*/
