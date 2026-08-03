ba35355666d340466bc0eb1d99711f08203f0ec9
RAHUL YADAV <rairahul5505@gmail.com>
2026-08-04 00:22:37 +0530

    Time: 5 ms (27.82%) | Memory: 51.3 MB (92.60%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,5 +1,46 @@
 class Solution {
     public static int numIslands(char[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int count = 0;
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<col; j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    bfs(grid,i,j,row,col);
+                }
+            }
+        }
+        return count;
+    }
+    public static void bfs(char grid[][],int sr,int sc,int m,int n){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sr,sc});
+        grid[sr][sc] = '0';
+        int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int rows = curr[0];
+            int cols = curr[1];
+            for(int[] direction : directions){
+                int newRow = direction[0] + rows;
+                int newCol = direction[1] + cols;
+                if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n &&
+                    grid[newRow][newCol] == '1'){
+                    grid[newRow][newCol] = '0';
+                    q.offer(new int[]{newRow,newCol});
+                    }
+            }
+        }
+    }
+}
+
+
+
+
+/*
+class Solution {
+    public static int numIslands(char[][] grid) {
        int m = grid.length;
        int n = grid[0].length;
        boolean[][] visited = new boolean[m][n];
@@ -25,4 +66,4 @@ class Solution {
     bfs(grid,row-1,col,visited,m,n);
     }
 }
-    
+*/   
