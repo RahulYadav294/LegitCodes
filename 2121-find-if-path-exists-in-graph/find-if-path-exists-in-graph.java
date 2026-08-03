f0b8e88f6fc172c0008f6f95ce505190bc2ac22b
RAHUL YADAV <rairahul5505@gmail.com>
2026-08-04 00:42:21 +0530

    Time: 126 ms (56.43%) | Memory: 280.6 MB (62.35%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,3 +1,42 @@
+/*
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<List<Integer>> graph = new ArrayList<>();
+        boolean[] visited = new boolean[n];
+        for(int i = 0; i<n; i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+         return bfs(graph,visited,source,destination);
+        
+    }
+    public boolean bfs(List<List<Integer>> graph,boolean[] visited,int src,int dst){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(src);
+        visited[src] = true;
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            if(curr == dst) return true;
+            for(int edge  : graph.get(curr)){
+                if(!visited[edge]){
+                    visited[curr] = true;
+                    q.add(edge);
+                }
+            }
+        }
+        return false;
+    }
+}
+*/
+
+
+
+
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         List<List<Integer>> graph = new ArrayList<>();
