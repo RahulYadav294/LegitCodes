1867dfa939e61d70513290da6678875915820766
RAHUL YADAV <rairahul5505@gmail.com>
2026-08-03 23:42:11 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (25.26%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,9 +1,35 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int row = image.length;
+        int col = image[0].length;
+        int orgColor = image[sr][sc];
+        if (orgColor == color) {
+            return image;
+        }
+        helper(image,sr,sc,row,col,orgColor,color);
+        return image;
+    }
+    public void helper(int[][] image,int sr,int sc,int row,int col,int orgColor,int color){
+        if(sr < 0 || sr >= row || sc < 0 || sc >= col || image[sr][sc] != orgColor){
+            return;
+        }
+        image[sr][sc] = color;
+        helper(image,sr+1,sc,row,col,orgColor,color);
+        helper(image,sr,sc+1,row,col,orgColor,color);
+        helper(image,sr-1,sc,row,col,orgColor,color);
+        helper(image,sr,sc-1,row,col,orgColor,color);
+    }
+}
+
+
+/*
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
        int m = image.length;
        int n = image[0].length;
        int orgColor = image[sr][sc];
         boolean[][] visited = new boolean[m][n];
+        
         dfs(image,sr,sc,orgColor,color,visited,m,n);
         return image; 
     }
@@ -20,3 +46,4 @@ class Solution {
 
     }
 }
+*/
