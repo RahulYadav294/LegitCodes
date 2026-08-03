eac8bf08d3c18498ca47fb21068fa56e0ce72a26
RAHUL YADAV <rairahul5505@gmail.com>
2026-08-03 16:37:46 +0530

    Time: 127 ms (94.02%) | Memory: 53.8 MB (70.12%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.java
@@ -1,6 +1,30 @@
 class Solution {
     public List<List<Integer>> findPrimePairs(int n) {
         List<List<Integer>> bigList = new ArrayList<>();
+        boolean[] sieve = new boolean[n+1];
+        Arrays.fill(sieve,true);
+        sieve[0] = false;
+        sieve[1] = false;
+        for(int i = 2; i*i<=n; i++){
+            if(sieve[i]){
+            for(int j = i * i; j<=n; j+=i){
+            sieve[j] = false;
+            }
+        }}
+        for(int i = 2; i<=n/2; i++){
+            if(sieve[i] && sieve[n-i]){
+                bigList.add(Arrays.asList(i,n-i));
+            }
+        }
+        return bigList;
+
+    }
+}
+    
+/*
+class Solution {
+    public List<List<Integer>> findPrimePairs(int n) {
+        List<List<Integer>> bigList = new ArrayList<>();
         for(int i = 2; i<=n/2; i++){
             int j = n-i;
                 if(isPrime(i) && isPrime(j)){
@@ -20,6 +44,7 @@ class Solution {
         return true;
     }
 }
+*/
 /*
 class Solution {
     public List<List<Integer>> findPrimePairs(int n) {
