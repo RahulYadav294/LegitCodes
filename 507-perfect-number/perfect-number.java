8ca855a1411c3891f3e4ba1523d9f243ecbb1b6e
RAHUL YADAV <rairahul5505@gmail.com>
2026-08-08 00:05:11 +0530

    Time: 1 ms (94.69%) | Memory: 42 MB (63.24%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,9 +1,24 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
+        if(num == 1) return false;
      int sum = 0;
-     for(int i = 1; i<=num/2; i++){
-        if(num % i == 0) sum+=i;
-     }
+     for(int i = 1; i*i<=num; i++){
+            if(num % i == 0){
+                if(num/i == i ){
+                    sum+=i;
+                }else{
+                    if(num/i == num){
+                        sum+=i;
+                    }else{
+                        sum+=i;
+                        sum+=num/i;;
+                    }
+                }
+                
+            }
+        }
+
+    
      return sum == num;
 
     }
