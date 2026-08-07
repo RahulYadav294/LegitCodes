6c6c1332fe9fc8ae24dd15c7e13359d8c652237f
RAHUL YADAV <rairahul5505@gmail.com>
2026-08-08 04:19:38 +0530

    Time: 3 ms (91.71%) | Memory: 46.6 MB (60.09%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> bigList = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
-        helper(nums,bigList,list,visited,0);
+        helper(nums,bigList,list,visited);
         return bigList;
     }
-    public void helper(int[] nums,  List<List<Integer>> bigList, List<Integer> list,  boolean[] visited,int index){
-        if(list.size() == nums.length){
-            if(!bigList.contains(list)){
+    public void helper(int[] nums,  List<List<Integer>> bigList, List<Integer> list,  boolean[] visited){
+            if(list.size() == nums.length){
             bigList.add(new ArrayList<>(list));
             return;
-            }
         }
         for(int i = 0; i<nums.length; i++){
+            if(i > 0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
             if(visited[i]) continue;
             visited[i] = true;
             list.add(nums[i]);
-            helper(nums,bigList,list,visited,index+1);
+            helper(nums,bigList,list,visited);
             list.remove(list.size()-1); 
              visited[i] = false; 
         }
