31a2c9dcc94a9b9dc0acd011891dd82147a8d3c0
RAHUL YADAV <rairahul5505@gmail.com>
2026-08-16 16:39:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (24.00%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newD = new int[n + 1];
+        newD[0] = 1;
+
+        return newD;
+    }
+}
