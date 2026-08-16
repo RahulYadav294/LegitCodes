4f4f30d4f525aceb9a3319ca995843d7fd49af18
RAHUL YADAV <rairahul5505@gmail.com>
2026-08-16 19:26:43 +0530

    Time: 18 ms (90.08%) | Memory: 45.9 MB (32.00%) - LeetSync

---
+++ 18-4sum/4sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i<n-3; i++){
+            if(i > 0 && nums[i] == nums[i-1])continue;
+            int first = nums[i];
+            for(int j = i+1; j<n-2; j++){
+                if(j > i+1 && nums[j] == nums[j-1])continue;
+            int second = nums[j];
+            int low = j+1;
+            int high = n-1;
+            while(low < high){
+                long sum = (long)first + second + nums[low] + nums[high];
+                if(sum == target){
+                    list.add(Arrays.asList(first,second,nums[low],nums[high]));
+                    low++;
+                    high--;
+                    while(low < high && nums[low] == nums[low-1]){low++;}
+                     while(low < high && nums[high] == nums[high+1]){high--;}
+                }else if(sum > target){high--;}else{low++;}
+            }
+        }
+        }
+        return list;
+    }
+}
