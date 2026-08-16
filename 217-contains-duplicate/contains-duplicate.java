5c1dd393ca5148938aa6b1f430d1b008d31e4edb
RAHUL YADAV <rairahul5505@gmail.com>
2026-08-16 16:18:26 +0530

    Time: 44 ms (5.12%) | Memory: 111.2 MB (5.66%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+        for(int num : map.values()){
+            if(num >= 2) return true;
+        }
+        return false;
+    }
+}
