c21f3b2b00896b68401e19843f8e14dbbcf0a6aa
RAHUL YADAV <rairahul5505@gmail.com>
2026-08-22 03:25:30 +0530

    Time: 47 ms (5.05%) | Memory: 48.1 MB (5.80%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,3 +1,44 @@
+
+class Solution {
+    public static String convert(String s, int numRows) {
+        if(numRows == 1 || s.length() <= numRows) return s;
+        char[][] matrix = new char[numRows][s.length()];
+        int row = 0; 
+        int col = 0;
+        boolean goingDown = true;
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            matrix[row][col] = ch;
+            if(goingDown){
+                if(row == numRows-1){
+                    goingDown = false;
+                    row--; col++;
+                }else{
+                    row++;
+                }
+            }else{
+                if( row == 0){
+                    goingDown = true;
+                    row++; 
+                }else{
+                   row--; col++;
+                }
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (int r = 0; r < numRows; r++) {
+            for (int c = 0; c < s.length(); c++) {
+                if (matrix[r][c] != 0) {
+                    result.append(matrix[r][c]);
+                }
+            }
+        }
+        return result.toString();
+    }
+}
+
+
+/*
 class Solution {
     public static String convert(String s, int numRows) {
         if (numRows == 1 || s.length() <= numRows) return s;
@@ -39,11 +80,5 @@ class Solution {
 
         return result.toString();
     }
-
-    public static void main(String[] args) {
-        String s = "PAYPALISHIRING";
-        int numRows = 3;
-        String result = convert(s, numRows);
-        System.out.print(result);  
-    }
 }
+*/
