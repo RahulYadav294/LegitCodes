3c65d312dedeabaf49ab996628343a46cdaf88af
RAHUL YADAV <rairahul5505@gmail.com>
2026-09-08 09:46:21 +0530

    Time: 1 ms (83.65%) | Memory: 42.5 MB (57.08%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countCommas(int n) {
+
+        long ans = 0;
+
+        if (n >= 1000)
+            ans += n - 999;
+
+        if (n >= 1_000_000)
+            ans += n - 999_999;
+
+        if (n >= 1_000_000_000)
+            ans += n - 999_999_999;
+
+        return (int)ans;
+    }
+}
